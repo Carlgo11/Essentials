b68f407d3b0fa7c699d6ef74791a2ef95e3b4c9c
Carlgo11 <carlgo11@carlgo11.com>
2014-12-23 00:31:17 +0100

    change tps to 20 when above 20

--- Essentials/src/com/earth2me/essentials/commands/Commandgc.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandgc.java
@@ -1,13 +1,11 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n.tl;
+import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.utils.DateUtil;
 import com.earth2me.essentials.utils.NumberUtil;
 import java.lang.management.ManagementFactory;
 import java.util.List;
-import java.util.logging.Level;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.Server;
@@ -25,6 +23,10 @@ public class Commandgc extends EssentialsCommand
 	protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception
 	{
 		double tps = ess.getTimer().getAverageTPS();
+        if (tps >= 20.0) {
+        tps = 20.0;
+        }
+		
 		ChatColor color;
 		if (tps >= 18.0)
 		{
@@ -39,11 +41,11 @@ public class Commandgc extends EssentialsCommand
 			color = ChatColor.RED;
 		}
 
-		sender.sendMessage(tl("uptime", DateUtil.formatDateDiff(ManagementFactory.getRuntimeMXBean().getStartTime())));
-		sender.sendMessage(tl("tps", "" + color + NumberUtil.formatDouble(tps)));
-		sender.sendMessage(tl("gcmax", (Runtime.getRuntime().maxMemory() / 1024 / 1024)));
-		sender.sendMessage(tl("gctotal", (Runtime.getRuntime().totalMemory() / 1024 / 1024)));
-		sender.sendMessage(tl("gcfree", (Runtime.getRuntime().freeMemory() / 1024 / 1024)));
+		sender.sendMessage(_("uptime", DateUtil.formatDateDiff(ManagementFactory.getRuntimeMXBean().getStartTime())));
+		sender.sendMessage(_("tps", "" + color + NumberUtil.formatDouble(tps)));
+		sender.sendMessage(_("gcmax", (Runtime.getRuntime().maxMemory() / 1024 / 1024)));
+		sender.sendMessage(_("gctotal", (Runtime.getRuntime().totalMemory() / 1024 / 1024)));
+		sender.sendMessage(_("gcfree", (Runtime.getRuntime().freeMemory() / 1024 / 1024)));
 
 		List<World> worlds = server.getWorlds();
 		for (World w : worlds)
@@ -58,22 +60,14 @@ public class Commandgc extends EssentialsCommand
 				worldType = "The End";
 				break;
 			}
-
+			
 			int tileEntities = 0;
-
-			try
-			{
-				for (Chunk chunk : w.getLoadedChunks())
-				{
-					tileEntities += chunk.getTileEntities().length;
-				}
-			}
-			catch (java.lang.ClassCastException ex)
-			{
-				Bukkit.getLogger().log(Level.SEVERE, "Corrupted chunk data on world " + w, ex);
+			
+			for (Chunk chunk : w.getLoadedChunks()) {
+				tileEntities += chunk.getTileEntities().length;
 			}
 
-			sender.sendMessage(tl("gcWorld", worldType, w.getName(), w.getLoadedChunks().length, w.getEntities().size(), tileEntities));
+			sender.sendMessage(_("gcWorld", worldType, w.getName(), w.getLoadedChunks().length, w.getEntities().size(), tileEntities));
 		}
 	}
 }
